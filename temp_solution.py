a743fdaa80a224a23d35beb7db0af0d6afd1c3e1
Vishal <vv5456462@gmail.com>
2026-09-19 10:56:43 +0530

    Stop tracking temp/editor files

--- temp_solution.py
+++
@@ -1,9 +0,0 @@
-def solution(a, b):
-    return a + b
-
-assert solution(1, 2) == 3
-assert solution(-5, 5) == 0
-assert solution(0, 0) == 0
-assert solution(123456789, 987654321) == 1111111110
-assert solution(1.5, 2.5) == 4.0
-print("ALL TESTS PASSED")
